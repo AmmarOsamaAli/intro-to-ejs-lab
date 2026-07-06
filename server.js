b6da322ea97ca7f402ba6498ce0da4730996e12a
AmmarOsamaAli <ammarosama080@icloud.com>
2026-07-06 16:10:41 +0300

    Lab Complete Plus Bonus Content

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-const express = require ('express')
+const express = require('express')
 
 const app = express()
 
+const resturants = require('./data')
+
+
+app.get('/', (req, res) => {
+    res.render('homepage.ejs')
+})
+
+
+app.get('/resturants', (req, res) => {
+    res.render('all-resturants.ejs', { resturants })
+})
+
+
+app.get('/resturants/:resturantsId', (req, res) => {
+    const foundResturnat = resturants.find(oneResturants => {
+        return oneResturants.id === Number(req.params.resturantsId)
+    })
+    res.render('resturant-details.ejs', { resturants: foundResturnat , menu : foundResturnat.menu })
+})
 
 
 
-
-
-
-
-
-
-app.listen(3000,()=>{
-    console.log("App is Runnin")
+app.listen(3000, () => {
+    console.log("App is Running")
 })
